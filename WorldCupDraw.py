4d584532f9d49b22a7d1d7b05328b1047cc8baf0
samwcounsell <78562860+samwcounsell@users.noreply.github.com>
2021-02-06 15:49:11 +0000

    Update WorldCupDraw.py

--- WorldCupDraw.py
+++ WorldCupDraw.py
@@ -36,24 +36,29 @@ groupH = potbig.iloc[[7, 15, 23, 31], :]
 groupH = groupH.set_index([pd.Index([0, 1, 2, 3]), ])
 
 for group in [groupA, groupB, groupC, groupD, groupE, groupF, groupG, groupH]:
-    for i, j in zip((0, 0, 0, 1, 1, 2), (1, 2, 3, 2, 3, 3)):
-        if i < j:
+    for i, j in zip((0, 1, 3, 2, 0, 2), (3, 2, 1, 0, 1, 3)):
+        if i != j:
+
             team1 = group.loc[i, 'Country']
             team2 = group.loc[j, 'Country']
             a1, d1 = group.loc[i, 'Attack'], group.loc[i, 'Defence']
             a2, d2 = group.loc[j, 'Attack'], group.loc[j, 'Defence']
+
             p1 = 0.014 * (a1 / d2)
             q1 = 1 - p1
             p2 = 0.014 * (a2 / d1)
             q2 = 1 - p2
+
             quantile = np.arange(0.01, 1, 0.1)
             Ber1 = bernoulli.rvs(p1, q1, size=90)
             Ber2 = bernoulli.rvs(p2, q2, size=90)
             goals1 = sum(Ber1)
             goals2 = sum(Ber2)
+
             print(team1, goals1, " - ", goals2, team2)
             # print(team1, "xG/m", format(p1, ".4f"), " xG/norm", format((p1 / 0.014), ".4f"), "xG_total",
             # format((p1 * 90), ".4f"))
             # print(team2, "xG/m", format(p2, ".4f"), " xG/norm", format((p2 / 0.014), ".4f"), "xG_total",
             # format((p2 * 90), ".4f"))
+
     print("################")
