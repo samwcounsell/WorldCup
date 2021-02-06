c17cb01d7f6b1197293b78d0543b710f55a75587
samwcounsell <78562860+samwcounsell@users.noreply.github.com>
2021-02-06 11:56:27 +0000

    Add files via upload

---
+++ WorldCupDraw.py
@@ -0,0 +1,46 @@
+from scipy.stats import binom, bernoulli
+import numpy as np
+import pandas as pd
+import random
+
+pot_data = pd.read_csv(r"C:\Users\samwc\PycharmProjects\WorldCup\PotTest.csv")
+pot_data = pot_data.sort_values(by=['World_Rank'])
+
+pot1 = pot_data.iloc[:8, :]
+pot2 = pot_data.iloc[9:16, :]
+pot3 = pot_data.iloc[17:24, :]
+pot4 = pot_data.iloc[25:32, :]
+
+pot1 = pot1.sample(frac=1)
+pot2 = pot2.sample(frac=1)
+pot3 = pot3.sample(frac=1)
+pot4 = pot4.sample(frac=1)
+print(pot1)
+
+potbig = pd.concat([pot1, pot2, pot3, pot4])
+print(potbig)
+
+groupA = potbig.iloc[[0, 7, 15, 23], :]
+groupA = groupA.set_index([pd.Index([0, 1, 2, 3]), ])
+print(groupA)
+
+for i, j in zip((0, 0, 0, 1, 1, 2), (1, 2, 3, 2, 3, 3)):
+    if i < j:
+        team1 = groupA.loc[i, 'Country']
+        team2 = groupA.loc[j, 'Country']
+        a1, d1 = groupA.loc[i, 'Attack'], groupA.loc[i, 'Defence']
+        a2, d2 = groupA.loc[j, 'Attack'], groupA.loc[j, 'Defence']
+        p1 = 0.014 * (a1 / d2)
+        q1 = 1 - p1
+        p2 = 0.014 * (a2 / d1)
+        q2 = 1 - p2
+        quantile = np.arange(0.01, 1, 0.1)
+        Ber1 = bernoulli.rvs(p1, q1, size=90)
+        Ber2 = bernoulli.rvs(p2, q2, size=90)
+        goals1 = sum(Ber1)
+        goals2 = sum(Ber2)
+        print(team1, goals1, " - ", goals2, team2)
+        print(team1, "xG/m", format(p1, ".4f"), " xG/norm", format((p1 / 0.014), ".4f"), "xG_total",
+              format((p1 * 90), ".4f"))
+        print(team2, "xG/m", format(p2, ".4f"), " xG/norm", format((p2 / 0.014), ".4f"), "xG_total",
+              format((p2 * 90), ".4f"))
