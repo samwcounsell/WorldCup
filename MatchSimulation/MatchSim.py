2b1615dae746725951ec14a56a08b0efc6896776
samwcounsell <78562860+samwcounsell@users.noreply.github.com>
2021-02-12 15:20:41 +0000

    Update MatchSim.py

--- MatchSimulation/MatchSim.py
+++ MatchSimulation/MatchSim.py
@@ -240,3 +240,70 @@ def GRP5(alpha):
 
             time.sleep(0.3)
     return alpha
+
+
+def GRP6(alpha):
+    for k in (1, 2):
+
+        a, b, c, d, e, f = 0, 1, 2, 3, 4, 5
+
+        for m in range(5):
+
+            print("\n", "MATCHDAY", m + ((k - 1) * 5) + 1)
+
+            for g in range(3):
+                if g == 0:
+                    t1 = a
+                    t2 = b
+                if g == 1:
+                    t1 = c
+                    t2 = d
+                if g == 2:
+                    t1 = e
+                    t2 = f
+
+                team1 = alpha.loc[t1, 'Country']
+                team2 = alpha.loc[t2, 'Country']
+                a1, d1 = alpha.loc[t1, 'Attack'], alpha.loc[t1, 'Defence']
+                a2, d2 = alpha.loc[t2, 'Attack'], alpha.loc[t2, 'Defence']
+
+                if k == 1:  # the * 1.2 gives the home team and advantage
+                    p1 = 0.014 * (a1 / d2) * 1.2
+                    p2 = 0.014 * (a2 / d1)
+                if k == 2:  # the * 1.2 gives the home team and advantage
+                    p1 = 0.014 * (a1 / d2)
+                    p2 = 0.014 * (a2 / d1) * 1.2
+
+                Ber1 = bernoulli.rvs(p1, size=90)
+                Ber2 = bernoulli.rvs(p2, size=90)
+                goals1 = sum(Ber1)
+                goals2 = sum(Ber2)
+
+                print(team1, goals1, " - ", goals2, team2)
+
+                alpha.loc[t1, 'P'], alpha.loc[t2, 'P'] = alpha.loc[t1, 'P'] + 1, alpha.loc[t2, 'P'] + 1
+                alpha.loc[t1, 'GF'], alpha.loc[t1, 'GA'] = alpha.loc[t1, 'GF'] + goals1, alpha.loc[t1, 'GA'] + goals2
+                alpha.loc[t2, 'GF'], alpha.loc[t2, 'GA'] = alpha.loc[t2, 'GF'] + goals2, alpha.loc[t2, 'GA'] + goals1
+                alpha.loc[t1, 'GD'] = alpha.loc[t1, 'GF'] - alpha.loc[t1, 'GA']
+                alpha.loc[t2, 'GD'] = alpha.loc[t2, 'GF'] - alpha.loc[t2, 'GA']
+
+                if goals1 > goals2:
+                    alpha.loc[t1, 'Pts'] = alpha.loc[t1, 'Pts'] + 3
+                    alpha.loc[t1, 'W'], alpha.loc[t2, 'L'] = alpha.loc[t1, 'W'] + 1, alpha.loc[t2, 'L'] + 1
+
+                if goals1 < goals2:
+                    alpha.loc[t2, 'Pts'] = alpha.loc[t2, 'Pts'] + 3
+                    alpha.loc[t2, 'W'], alpha.loc[t1, 'L'] = alpha.loc[t2, 'W'] + 1, alpha.loc[t1, 'L'] + 1
+
+                if goals1 == goals2:
+                    alpha.loc[t1, 'Pts'] = alpha.loc[t1, 'Pts'] + 1
+                    alpha.loc[t2, 'Pts'] = alpha.loc[t2, 'Pts'] + 1
+                    alpha.loc[t1, 'D'], alpha.loc[t2, 'D'] = alpha.loc[t1, 'D'] + 1, alpha.loc[t2, 'D'] + 1
+
+                #time.sleep(0.3)
+
+            nb, nc, nd, ne, nf = c, e, b, f, d
+            b, c, d, e, f = nb, nc, nd, ne, nf
+
+            #time.sleep(0.3)
+    return alpha
