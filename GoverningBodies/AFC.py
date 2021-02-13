69e93f2f8b1ae10f942424723ebcc61158e61613
githubkeano <78621277+githubkeano@users.noreply.github.com>
2021-02-13 17:39:03 +0000

    Update AFC.py

--- GoverningBodies/AFC.py
+++ GoverningBodies/AFC.py
@@ -136,70 +136,7 @@ for group in [groupA, groupB]:
     print(group)
     #time.sleep(1)
 
-    for k in (1, 2):
-
-        a, b, c, d, e, f = 0, 1, 2, 3, 4, 5
-
-        for m in range(5):
-
-            print("\n", "MATCHDAY", m + ((k - 1) * 5) + 1)
-
-            for g in range(3):
-                if g == 0:
-                    t1 = a
-                    t2 = b
-                if g == 1:
-                    t1 = c
-                    t2 = d
-                if g == 2:
-                    t1 = e
-                    t2 = f
-
-                team1 = group.loc[t1, 'Country']
-                team2 = group.loc[t2, 'Country']
-                a1, d1 = group.loc[t1, 'Attack'], group.loc[t1, 'Defence']
-                a2, d2 = group.loc[t2, 'Attack'], group.loc[t2, 'Defence']
-
-                if k == 1:  # the * 1.2 gives the home team and advantage
-                    p1 = 0.014 * (a1 / d2) * 1.2
-                    p2 = 0.014 * (a2 / d1)
-                if k == 2:  # the * 1.2 gives the home team and advantage
-                    p1 = 0.014 * (a1 / d2)
-                    p2 = 0.014 * (a2 / d1) * 1.2
-
-                Ber1 = bernoulli.rvs(p1, size=90)
-                Ber2 = bernoulli.rvs(p2, size=90)
-                goals1 = sum(Ber1)
-                goals2 = sum(Ber2)
-
-                print(team1, goals1, " - ", goals2, team2)
-
-                group.loc[t1, 'P'], group.loc[t2, 'P'] = group.loc[t1, 'P'] + 1, group.loc[t2, 'P'] + 1
-                group.loc[t1, 'GF'], group.loc[t1, 'GA'] = group.loc[t1, 'GF'] + goals1, group.loc[t1, 'GA'] + goals2
-                group.loc[t2, 'GF'], group.loc[t2, 'GA'] = group.loc[t2, 'GF'] + goals2, group.loc[t2, 'GA'] + goals1
-                group.loc[t1, 'GD'] = group.loc[t1, 'GF'] - group.loc[t1, 'GA']
-                group.loc[t2, 'GD'] = group.loc[t2, 'GF'] - group.loc[t2, 'GA']
-
-                if goals1 > goals2:
-                    group.loc[t1, 'Pts'] = group.loc[t1, 'Pts'] + 3
-                    group.loc[t1, 'W'], group.loc[t2, 'L'] = group.loc[t1, 'W'] + 1, group.loc[t2, 'L'] + 1
-
-                if goals1 < goals2:
-                    group.loc[t2, 'Pts'] = group.loc[t2, 'Pts'] + 3
-                    group.loc[t2, 'W'], group.loc[t1, 'L'] = group.loc[t2, 'W'] + 1, group.loc[t1, 'L'] + 1
-
-                if goals1 == goals2:
-                    group.loc[t1, 'Pts'] = group.loc[t1, 'Pts'] + 1
-                    group.loc[t2, 'Pts'] = group.loc[t2, 'Pts'] + 1
-                    group.loc[t1, 'D'], group.loc[t2, 'D'] = group.loc[t1, 'D'] + 1, group.loc[t2, 'D'] + 1
-
-                #time.sleep(0.3)
-
-            nb, nc, nd, ne, nf = c, e, b, f, d
-
-            b, c, d, e, f = nb, nc, nd, ne, nf
-
-            #time.sleep(0.3)
+    group = GRP6(group)
 
     #time.sleep(1)
     group = group.sort_values(['Pts', 'GD', 'GF', 'GA'], ascending=[False, False, False, True])
@@ -212,120 +149,15 @@ for group in [groupA, groupB]:
     round4 = group.iloc[2:3, :]
     pot_data = pd.concat([pot_data, round4])
 
-    uc = input("Do you wish to continue, 1 if yes, 0 if no: ")  # uc = user continue
-    if uc == 0:
-        sys.exit
+    uc = input("Press enter to continue: ")
 
-round4 = pot_data.iloc[12:, :]
-round4 = round4.sample(frac=1)
-round4 = round4.set_index([pd.Index([0, 1]), ])
 pot_data = pot_data.iloc[12:, :]
+pot_data = pot_data.set_index([pd.Index([0, 1]), ])
 
-print("############   ROUND 4  ############")
-for a in range(1):
-    team1 = round4.loc[(2 * a), 'Country']
-    team2 = round4.loc[(2 * a + 1), 'Country']
-    a1, d1 = round4.loc[(2 * a), 'Attack'], round4.loc[(2 * a), 'Defence']
-    a2, d2 = round4.loc[(2 * a), 'Attack'], round4.loc[(2 * a), 'Defence']
-    for leg in (1, 2):
-        if leg == 1:
-            p1 = 0.014 * (a1 / d2)
-            p2 = 0.014 * (a2 / d1) * 0.8
-            Ber1 = bernoulli.rvs(p1, size=90)
-            Ber2 = bernoulli.rvs(p2, size=90)
-            hgoals1 = sum(Ber1)
-            agoals2 = sum(Ber2)
-            print(team1, hgoals1, " - ", agoals2, team2)
-            #time.sleep(1)
+print("\nROUND 4\n")
 
-        if leg == 2:
-            p1 = 0.014 * (a1 / d2) * 0.8
-            p2 = 0.014 * (a2 / d1)
-            Ber1 = bernoulli.rvs(p1, size=90)
-            Ber2 = bernoulli.rvs(p2, size=90)
-            agoals1 = sum(Ber1)
-            hgoals2 = sum(Ber2)
-            goals1 = hgoals1 + agoals1
-            goals2 = hgoals2 + agoals2
-            print(team1, agoals1, "[", goals1, "]", " - ", "[", goals2, "]", hgoals2, team2)
-
-    if goals1 > goals2:
-        winner = team1
-        ict = round4.iloc[(2 * a):(2 * a + 1), :]
-
-    if goals1 < goals2:
-        winner = team2
-        ict = round4.iloc[(2 * a + 1):(2 * a + 2), :]
-
-    if goals1 == goals2:
-        if agoals1 > agoals2:
-            winner = team1
-            ict = round4.iloc[(2 * a):(2 * a + 1), :]
-
-        if agoals2 > agoals1:
-            winner = team2
-            ict = round4.iloc[(2 * a + 1):(2 * a + 2), :]
-
-        if agoals1 == agoals2:
-            #time.sleep(1)
-            Ber1 = bernoulli.rvs(p1, size=30)
-            Ber2 = bernoulli.rvs(p2, size=30)
-            goals1 = sum(Ber1) + goals1
-            goals2 = sum(Ber2) + goals2
-
-            if goals1 > goals2:
-                winner = team1
-                ict = round4.iloc[(2 * a):(2 * a + 1), :]
-
-                print("ET:", team1, goals1, " - ", goals2, team2)
-
-            if goals1 < goals2:
-                winner = team2
-                ict = round4.iloc[(2 * a + 1):(2 * a + 2), :]
-
-                print("ET:", team1, goals1, " - ", goals2, team2)
-
-            if goals1 == goals2:
-                print("ET:", team1, goals1, " - ", goals2, team2)
-                #time.sleep(1)
-                Ber1 = bernoulli.rvs(0.7, size=5)
-                Ber2 = bernoulli.rvs(0.7, size=5)
-                pen1 = sum(Ber1)
-                pen2 = sum(Ber2)
-
-                if pen1 > pen2:
-                    winner = team1
-                    ict = round4.iloc[(2 * a):(2 * a + 1), :]
-
-                    print(team1, goals1, "(", pen1, ") - (", pen2, ")", goals2, team2)
-                if pen1 < pen2:
-                    winner = team2
-                    ict = round4.iloc[(2 * a + 1):(2 * a + 2), :]
-
-                    print(team1, goals1, "(", pen1, ") - (", pen2, ")", goals2, team2)
-                if pen1 == pen2:
-                    for b in range(100):
-                        Ber1 = bernoulli.rvs(0.7, size=1)
-                        Ber2 = bernoulli.rvs(0.7, size=1)
-                        pen1 = sum(Ber1) + pen1
-                        pen2 = sum(Ber2) + pen2
-                        if pen1 != pen2:
-                            if pen1 > pen2:
-                                winner = team1
-                                ict = round4.iloc[(2 * a):(2 * a + 1), :]
-
-                                print(team1, goals1, "(", pen1, ") - (", pen2, ")", goals2, team2)
-                                break
-                            if pen1 < pen2:
-                                winner = team2
-                                ict = round4.iloc[(2 * a + 1):(2 * a + 2), :]
-
-                                print(team1, goals1, "(", pen1, ") - (", pen2, ")", goals2, team2)
-                                break
-
-    #time.sleep(1)
-    print("############")
-
+ict = TLKO(1, pot_data, pot_data, 0.5)
+ict = ict.iloc[2:, :]
 ict = ict[['Country', 'World_Rank']]
 qualified = qualified[['Country', 'World_Rank']]
 qualified = qualified.iloc[1:, :]
@@ -333,5 +165,8 @@ print("\nQUALIFIED FOR WORLD CUP\n")
 print(qualified)
 print("\nQUALIFIED FOR INTERCONTINENTAL PLAYOFF\n")
 print(ict, "\n")
-uc = input("Press enter to exit: ")  # uc = user continue
 
+#afc = pd.concat([ict, qualified])
+#return afc
+
+uc = input("Press enter to exit: ")  # uc = user continue
