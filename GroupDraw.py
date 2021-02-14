a3678a36f1174f029047ab9947cc466236b8935e
samwcounsell <78562860+samwcounsell@users.noreply.github.com>
2021-02-14 12:37:04 +0000

    Update GroupDraw.py

--- GroupDraw.py
+++ GroupDraw.py
@@ -5,3 +5,9 @@ def GD4(i, n, df):
     a = df.iloc[[i, i + n, i + (2 * n), i + (3 * n)], :]
     a = a.set_index([pd.Index([0, 1, 2, 3]), ])
     return a
+
+
+def GD5(i, n, df):
+    a = df.iloc[[i, i + n, i + (2 * n), i + (3 * n), i + (4 * n)], :]
+    a = a.set_index([pd.Index([0, 1, 2, 3, 4]), ])
+    return a
