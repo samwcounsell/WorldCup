41ad8b41cbf966e63ebbd2a75254e7d6aa9700f2
githubkeano <78621277+githubkeano@users.noreply.github.com>
2021-02-14 16:01:27 +0000

    Update CONMEBOL.py

--- GoverningBodies/CONMEBOL.py
+++ GoverningBodies/CONMEBOL.py
@@ -12,18 +12,16 @@ from GroupDraw import GD4
 pot_data = pd.read_csv(r"/Users/keanerussell/Documents/Documents/Home/Python/CONMEBOL.csv")
 pot_data = pot_data.sort_values(by=['World_Rank'])
 
-for i in range(10):
-    #group = GD4(i, 1, pot_data)
-    group = pot_data
-    print("\n", group, "\n")
+group = pot_data
+print("\n", group, "\n")
 
-    group = GRP10(group)
-    group = group.sort_values(['Pts', 'GD', 'GF', 'GA'], ascending=[False, False, False, True])
-    group = group.reset_index()
-    group = group.drop(['index'], axis=1)
+group = GRP10(group)
+group = group.sort_values(['Pts', 'GD', 'GF', 'GA'], ascending=[False, False, False, True])
+group = group.reset_index()
+group = group.drop(['index'], axis=1)
     #round3 = group.iloc[0:1, :]
     #pot_data = pd.concat([pot_data, round3])
 
-    print("\n", group, "\n")
+print("\n", group, "\n")
 
-    uc = input("Press enter to continue: ")  # uc = user continue
+uc = input("Press enter to continue: ")  # uc = user continue
