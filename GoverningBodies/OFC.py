666ae34568152d64776ebb5aa091066fef4029de
githubkeano <78621277+githubkeano@users.noreply.github.com>
2021-02-14 19:13:44 +0000

    Update OFC.py

--- GoverningBodies/OFC.py
+++ GoverningBodies/OFC.py
@@ -34,20 +34,20 @@ group2 = group2.sort_values(['Pts', 'GD', 'GF', 'GA'], ascending=[False, False, 
 group2 = group2.reset_index()
 group2 = group2.drop(['index'], axis=1)
 print(group2)
-#uc = input("Press enter to continue: ")  # uc = user continue
+
+uc = input("Press enter to continue: ")  # uc = user continue
 
 print("\nROUND 2\n")
-
-winner1 = group1.iloc[0:2, :]
-winner2 = group2.iloc[0:2, :]
-winner = pd.concat([winner1, winner2])
-winner = winner.sample(frac=1)
+winner1 = group1.iloc[0:1, :]
+runnerup1 = group1.iloc[1:2, :]
+winner2 = group2.iloc[0:1, :]
+runnerup2 = group2.iloc[1:2, :]
+winner = pd.concat([winner1, runnerup2, winner2, runnerup1])
 winner = winner.set_index([pd.Index([0, 1, 2, 3]), ])
 winner = TLKO(2, winner, winner, 0.5)
-#uc = input("Press enter to continue: ")  # uc = user continue
+uc = input("Press enter to continue: ")  # uc = user continue
 
-print("ROUND 3\n")
-
+print("\nROUND 3\n")
 ict = winner.iloc[4:, :]
 ict = ict.set_index([pd.Index([0, 1]), ])
 ict = TLKO(1, ict, ict, 0.5)
