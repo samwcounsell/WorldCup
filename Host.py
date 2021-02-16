c244ff47acfdef7a82815e2b600df15bffc39205
samwcounsell <78562860+samwcounsell@users.noreply.github.com>
2021-02-16 17:38:43 +0000

    Add files via upload

--- Host.py
+++ Host.py
@@ -1,20 +1,12 @@
 import random
 import pandas as pd
 
-#def host():
-    # nations = ["England", "USA", "France", "Brazil", "China", "Australia", "South Africa", "India", "Japan", "Qatar"]
-    #nations = ["Japan", "South Korea"]
-    #host = random.choice(nations)
-    #print(host)
-
-    #return host
-
 def host():
     possiblehosts = pd.read_csv(r"C:\Users\samwc\PycharmProjects\WorldCup\Host.csv")
-    n = random.randint(0, 8)
+    n = random.randint(0, 16)
 
     host = possiblehosts.loc[n, 'Country']
-    print(host)
+    print("\n", host)
     hostdf = possiblehosts.iloc[n:n + 1, :]
 
     return host, hostdf
