f118b22bad98ba768815b0d465adcff2af471b27
githubkeano <78621277+githubkeano@users.noreply.github.com>
2021-02-18 17:21:27 +0000

    Update MatchGameSim.py

--- MatchSimulation/MatchGameSim.py
+++ MatchSimulation/MatchGameSim.py
@@ -5,7 +5,6 @@ import time
 import random
 import sys
 
-
 def TLGRP90(k, a1, d1, a2, d2):
     if k == 1:  # the * 1.2 gives the home team and advantage
         p1 = 0.014 * (a1 / d2) * 1.2
@@ -19,3 +18,17 @@ def TLGRP90(k, a1, d1, a2, d2):
     goals1 = sum(Ber1)
     goals2 = sum(Ber2)
     return goals1, goals2
+
+def TLGRP90HA(k, a1, d1, a2, d2):
+    if k == 1:  # the * 1.2 gives the home team and advantage
+        p1 = 0.014 * (a1 / d2) * 1.2
+        p2 = 0.014 * (a2 / d1)
+    if k == 2:  # the * 1.2 gives the home team and advantage
+        p1 = 0.014 * (a1 / d2) * 1.2
+        p2 = 0.014 * (a2 / d1)
+
+    Ber1 = bernoulli.rvs(p1, size=90)
+    Ber2 = bernoulli.rvs(p2, size=90)
+    goals1 = sum(Ber1)
+    goals2 = sum(Ber2)
+    return goals1, goa
