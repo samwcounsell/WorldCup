0866371f31682c642bf2b6cc14f00dace10aac3a
samwcounsell <78562860+samwcounsell@users.noreply.github.com>
2021-02-18 17:18:25 +0000

    Update CONMEBOL.py

--- GoverningBodies/CONMEBOL.py
+++ GoverningBodies/CONMEBOL.py
@@ -7,37 +7,42 @@ import sys
 from MatchSim import TLKO
 from MatchSim import GRP10
 from GroupDraw import GD4
-from Host import host
 
-pot_data = pd.read_csv("CONMEBOL.csv")
-pot_data = pot_data.sort_values(by=['World_Rank'])
+def conmebol():
+    from Host import hosty
 
-pot = pd.read_csv("UEFA.csv")
+    pot_data = pd.read_csv(r"C:\Users\samwc\PycharmProjects\WorldCup\CONMEBOL.csv")
+    # pot_data = pd.read_csv(r"/Users/keanerussell/Documents/Documents/Home/Python/CONMEBOL.csv")
+    pot_data = pot_data.sort_values(by=['World_Rank'])
 
-CONMEBOLhosts = ["Brazil", "Argentina"]
-host, hostdf = host()
+    pot = pd.read_csv("UEFA.csv")
 
-group = pot_data
-print("\n", group, "\n")
+    CONMEBOLhosts = ["Brazil", "Argentina"]
+    host, hostdf = hosty()
 
-group = GRP10(group)
-group = group.sort_values(['Pts', 'GD', 'GF', 'GA'], ascending=[False, False, False, True])
-group = group.reset_index()
-group = group.drop(['index'], axis=1)
+    group = pot_data
+    print("\n", group.to_string(columns=['Country', 'P', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Pts'], index=False), "\n")
 
-print("\n", group, "\n")
+    group = GRP10(group)
+    group = group.sort_values(['Pts', 'GD', 'GF', 'GA'], ascending=[False, False, False, True])
+    group = group.reset_index()
+    group = group.drop(['index'], axis=1)
 
-if host in CONMEBOLhosts:
-    group = group[group.Country != host]
+    print("\n", group.to_string(columns=['Country', 'P', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Pts'], index=False), "\n")
 
-qualified = group.iloc[:4, :]
-ict = group.iloc[4:5, :]
+    if host in CONMEBOLhosts:
+        group = group[group.Country != host]
 
-print("QUALIFIED FOR THE WORLD CUP\n")
-print(qualified.to_string(columns = ['Country'], index = False))
-print("\nQUALIFIED FOR INTERCONTINENTAL PLAYOFF\n")
-print(ict.to_string(columns = ['Country'], index = False), "\n")
-if host in CONMEBOLhosts:
-    print("\nQUALIFIED AS HOST\n")
-    print(host)
+    qualified = group.iloc[:4, :]
+    ict = group.iloc[4:5, :]
+
+    print("QUALIFIED FOR THE WORLD CUP\n")
+    print(qualified.to_string(columns=['Country'], index=False))
+    print("\nQUALIFIED FOR INTERCONTINENTAL PLAYOFF\n")
+    print(ict.to_string(columns=['Country'], index=False), "\n")
+    if host in CONMEBOLhosts:
+        print("\nQUALIFIED AS HOST\n")
+        print(host)
+
+    return (qualified, ict)
 
