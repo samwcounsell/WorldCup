00b228157e2b9a9229a4a720d85112d39e6ff9cb
samwcounsell <78562860+samwcounsell@users.noreply.github.com>
2021-02-18 17:19:29 +0000

    Update Host.py

--- Host.py
+++ Host.py
@@ -1,14 +1,14 @@
 import random
 import pandas as pd
 
-def host():
-    possiblehosts = pd.read_csv(r"C:\Users\samwc\PycharmProjects\WorldCup\Host.csv")
-    n = random.randint(0, 16)
+possiblehosts = pd.read_csv(r"C:\Users\samwc\PycharmProjects\WorldCup\Host.csv")
+n = random.randint(0, 20)
 
-    host = possiblehosts.loc[n, 'Country']
-    print("\n", host)
-    hostdf = possiblehosts.iloc[n:n + 1, :]
+host = possiblehosts.loc[n, 'Country']
+#print("\n", host)
+hostdf = possiblehosts.iloc[n:n + 1, :]
 
+def hosty():
     return host, hostdf
 
 
