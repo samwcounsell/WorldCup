eba88e7b4aaa48dae60b6c60eddd1c06bc678870
samwcounsell <78562860+samwcounsell@users.noreply.github.com>
2021-02-18 17:18:45 +0000

    Update OFC.py

--- GoverningBodies/OFC.py
+++ GoverningBodies/OFC.py
@@ -6,55 +6,57 @@ import random
 import sys
 from MatchSim import TLKO, GRP5, GRP6
 
-#pot_data = pd.read_csv(r"C:\Users\samwc\PycharmProjects\WorldCup\OFC.csv")
-pot_data = pd.read_csv(r"/Users/keanerussell/Documents/Documents/Home/Python/OFC.csv")
+def ofc():
+    pot_data = pd.read_csv(r"C:\Users\samwc\PycharmProjects\WorldCup\OFC.csv")
+    # pot_data = pd.read_csv(r"/Users/keanerussell/Documents/Documents/Home/Python/OFC.csv")
 
-print("\nWELCOME TO OFC WORLD CUP QUALIFYING\n")
-print("ROUND 1")
+    print("\nWELCOME TO OFC WORLD CUP QUALIFYING\n")
+    print("ROUND 1")
 
-pot_data = pot_data.sample(frac=1)
-pot_data = pot_data.reset_index()
-pot_data = pot_data.drop(['index'], axis=1)
+    pot_data = pot_data.sample(frac=1)
+    pot_data = pot_data.reset_index()
+    pot_data = pot_data.drop(['index'], axis=1)
 
-group1 = pot_data.iloc[0:5, :]
-group1 = group1.reset_index()
-group1 = group1.drop(['index'], axis=1)
-group2 = pot_data.iloc[5:11, :]
-group2 = group2.reset_index()
-group2 = group2.drop(['index'], axis=1)
+    group1 = pot_data.iloc[0:5, :]
+    group1 = group1.reset_index()
+    group1 = group1.drop(['index'], axis=1)
+    group2 = pot_data.iloc[5:11, :]
+    group2 = group2.reset_index()
+    group2 = group2.drop(['index'], axis=1)
 
-print("\nGROUP A")
-print(group1)
-group1 = GRP5(group1)
-group1 = group1.sort_values(['Pts', 'GD', 'GF', 'GA'], ascending=[False, False, False, True])
-group1 = group1.reset_index()
-group1 = group1.drop(['index'], axis=1)
-print("\n", group1)
+    print("\nGROUP A")
+    print(group1)
+    group1 = GRP5(group1)
+    group1 = group1.sort_values(['Pts', 'GD', 'GF', 'GA'], ascending=[False, False, False, True])
+    group1 = group1.reset_index()
+    group1 = group1.drop(['index'], axis=1)
+    print("\n", group1)
 
-print("\nGROUP B")
-print(group2)
-group2 = GRP6(group2)
-group2 = group2.sort_values(['Pts', 'GD', 'GF', 'GA'], ascending=[False, False, False, True])
-group2 = group2.reset_index()
-group2 = group2.drop(['index'], axis=1)
-print("\n", group2)
-uc = input("Press enter to continue: ")  # uc = user continue
+    print("\nGROUP B")
+    print(group2)
+    group2 = GRP6(group2)
+    group2 = group2.sort_values(['Pts', 'GD', 'GF', 'GA'], ascending=[False, False, False, True])
+    group2 = group2.reset_index()
+    group2 = group2.drop(['index'], axis=1)
+    print("\n", group2)
+    uc = input("Press enter to continue: ")  # uc = user continue
 
-print("\nROUND 2\n")
-winner1 = group1.iloc[0:1, :]
-runnerup1 = group1.iloc[1:2, :]
-winner2 = group2.iloc[0:1, :]
-runnerup2 = group2.iloc[1:2, :]
-winner = pd.concat([winner1, runnerup2, winner2, runnerup1])
-winner = winner.set_index([pd.Index([0, 1, 2, 3]), ])
-winner = TLKO(2, winner, winner, 0.5)
-uc = input("Press enter to continue: ")  # uc = user continue
+    print("\nROUND 2\n")
+    winner1 = group1.iloc[0:1, :]
+    runnerup1 = group1.iloc[1:2, :]
+    winner2 = group2.iloc[0:1, :]
+    runnerup2 = group2.iloc[1:2, :]
+    winner = pd.concat([winner1, runnerup2, winner2, runnerup1])
+    winner = winner.set_index([pd.Index([0, 1, 2, 3]), ])
+    winner = TLKO(2, winner, winner, 0.5)
+    uc = input("Press enter to continue: ")  # uc = user continue
 
-print("\nROUND 3\n")
-ict = winner.iloc[4:, :]
-ict = ict.set_index([pd.Index([0, 1]), ])
-ict = TLKO(1, ict, ict, 0.5)
-ict = ict.iloc[2:, :]
-ict = ict[['Country', 'World_Rank']]
-print("QUALIFIED FOR INTERCONTINENTAL PLAYOFF\n")
-print(ict)
+    print("\nROUND 3\n")
+    ict = winner.iloc[4:, :]
+    ict = ict.set_index([pd.Index([0, 1]), ])
+    ict = TLKO(1, ict, ict, 0.5)
+    ict = ict.iloc[2:, :]
+    print("QUALIFIED FOR INTERCONTINENTAL PLAYOFF\n")
+    print(ict)
+
+    return ict
