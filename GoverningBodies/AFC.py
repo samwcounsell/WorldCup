19eef6075da613ec7604fbc3db332db39aa4a593
samwcounsell <78562860+samwcounsell@users.noreply.github.com>
2021-02-22 18:08:49 +0000

    Update AFC.py

--- GoverningBodies/AFC.py
+++ GoverningBodies/AFC.py
@@ -3,40 +3,49 @@ import time
 from MatchSim import TLKO, GRP5, GRP6HA
 from GroupDraw import GD5
 
+# Start of alphabet is defined to name groups within the function
 alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
 
+
 def afc(time_delay):
+    # Importing host inside function so as to not generate a new host
     from Host import hosty
 
-    pot_data = pd.read_csv("AFC.csv")
-    pot_data = pot_data.sort_values(by=['World_Rank'])
+    # Reading in the AFC teams and sorting by world rank
+    team_data = pd.read_csv("AFC.csv")
+    team_data = team_data.sort_values(by=['World_Rank'])
 
+    # Listing possible AFC hosts, then calling the host nation
     AFChosts = ["China", "India", "Japan", "Qatar", "South Korea", "Saudi Arabia"]
     host, hostdf = hosty()
 
-    round1 = pot_data.iloc[34:46]
+    # Pulling last 12 teams to play in round 1, then removing them from the main data set
+    round1 = team_data.iloc[34:46]
     round1 = round1.sample(frac=1)
     round1 = round1.set_index([pd.Index([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]), ])
-    pot_data = pot_data.iloc[:34, :]
+    team_data = team_data.iloc[:34, :]
 
     print("\nWELCOME TO AFC WORLD CUP QUALIFYING\n")
     print("ROUND 1\n")
 
-    a = 6
-    pot_data = TLKO(a, round1, pot_data, 0.5)
+    # Running the two-leg knockout function, inputs: number of matches, dataset of participating teams, dataset to
+    # attach winners to, time delay
+    team_data = TLKO(6, round1, team_data, 0.5)
 
-    uc = input("Press enter to continue: ")  # uc = user continue
+    # Checking the user wants to continue
+    input("Press enter to continue: ")  # uc = user continue
 
-    pot_data = pot_data.sort_values(by=['World_Rank'])
-    pot_data = pot_data.reset_index()
-    pot_data = pot_data.drop(['index'], axis=1)
+    # Resorting the teams by world rank, the reindexing
+    team_data = team_data.sort_values(by=['World_Rank'])
+    team_data = team_data.reset_index()
+    team_data = team_data.drop(['index'], axis=1)
 
-    pot1 = pot_data.iloc[:8, :]
-    pot2 = pot_data.iloc[8:16, :]
-    pot3 = pot_data.iloc[16:24, :]
-    pot4 = pot_data.iloc[24:32, :]
-    pot5 = pot_data.iloc[32:40, :]
-
+    # Drawing the 5 pots, then frac = 1 randomises the teams so the draw is random
+    pot1 = team_data.iloc[:8, :]
+    pot2 = team_data.iloc[8:16, :]
+    pot3 = team_data.iloc[16:24, :]
+    pot4 = team_data.iloc[24:32, :]
+    pot5 = team_data.iloc[32:40, :]
     pot1 = pot1.sample(frac=1)
     pot2 = pot2.sample(frac=1)
     pot3 = pot3.sample(frac=1)
@@ -47,53 +56,64 @@ def afc(time_delay):
 
     print("\nROUND 2\n")
 
+    # Simulating the 8 groups of 5 teams, hence i in range 8
     for i in range(8):
+        # Calling the group draw function which draws the current group
         group = GD5(i, 5, potbig)
         print("\nGroup", alphabet[i])
         print(group.to_string(columns=['Country', 'P', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Pts'], index=False))
         time.sleep(1)
 
+        # Running the matches for the current group, the function returns the final group table
         group = GRP5(group)
 
+        # Sorting the group by points, then goal difference etc
         time.sleep(1)
         group = group.sort_values(['Pts', 'GD', 'GF', 'GA'], ascending=[False, False, False, True])
+
+        # Reindexing the group again, so we can pull the top two teams
         group = group.reset_index()
         group = group.drop(['index'], axis=1)
         print("\n", group.to_string(columns=['Country', 'P', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Pts'], index=False),
               "\n")
 
+        # Pulling the winner and runner up and attaching them to the main data frame
         round3 = group.iloc[0:2, :]
-        pot_data = pd.concat([pot_data, round3])
+        team_data = pd.concat([team_data, round3])
 
-        uc = input("Press enter to continue: ")  # uc = user continue
+        input("Press enter to continue: ")  # uc = user continue
 
         print()
 
-    ####### ROUND 3 ONWARDS BEWARE ############
+    # Removing the 40 teams that went into the previous stage, leaving us with only the top 2 from each group
+    team_data = team_data.iloc[40:, :]
 
-    pot_data = pot_data.iloc[40:, :]
+    # Separating the group winners
+    groupwinner = team_data.loc[0, :]
 
-    groupwinner = pot_data.loc[0, :]
-
-    runnerup = pot_data.loc[1, :]
+    # Selecting then sorting the runners up and then rejoining them to the winners
+    runnerup = team_data.loc[1, :]
     runnerup = runnerup.sort_values(by=['Pts'], ascending=False)
     hostcheck = pd.concat([groupwinner, runnerup])
 
+    # Making sure the host doesn't qualify by removing them if they're in the dataset, then selecting the winners and
+    # 4 best runners up to move onto round 3
     if host in AFChosts:
         hostcheck = hostcheck[hostcheck.Country != host]
-    pot_data = hostcheck.iloc[:12, :]
+    team_data = hostcheck.iloc[:12, :]
 
-    pot_data = pot_data.sort_values(by=['World_Rank'])
-    pot_data = pot_data.reset_index()
-    pot_data = pot_data.drop(['index'], axis=1)
+    # Sorting and reindexing again
+    team_data = team_data.sort_values(by=['World_Rank'])
+    team_data = team_data.reset_index()
+    team_data = team_data.drop(['index'], axis=1)
 
-    pot1 = pot_data.iloc[:2, :]
-    pot2 = pot_data.iloc[2:4, :]
-    pot3 = pot_data.iloc[4:6, :]
-    pot4 = pot_data.iloc[6:8, :]
-    pot5 = pot_data.iloc[8:10, :]
-    pot6 = pot_data.iloc[10:12, :]
-
+    # Redrawing and randomising pots
+    pot1 = team_data.iloc[:2, :]
+    pot2 = team_data.iloc[2:4, :]
+    pot3 = team_data.iloc[4:6, :]
+    pot4 = team_data.iloc[6:8, :]
+    pot5 = team_data.iloc[8:10, :]
+    pot6 = team_data.iloc[10:12, :]
     pot1 = pot1.sample(frac=1)
     pot2 = pot2.sample(frac=1)
     pot3 = pot3.sample(frac=1)
@@ -101,48 +121,64 @@ def afc(time_delay):
     pot5 = pot5.sample(frac=1)
     pot6 = pot6.sample(frac=1)
 
-    pot_data = pd.concat([pot1, pot2, pot3, pot4, pot5, pot6])
-    pot_data[['P', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Pts']] = 0
+    # Rejoining pots, resetting stats for the next group stage
+    team_data = pd.concat([pot1, pot2, pot3, pot4, pot5, pot6])
+    team_data[['P', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Pts']] = 0
 
-    groupA = pot_data.iloc[[0, 2, 4, 6, 8, 10], :]
+    # Drawing the 2 groups of 6, then setting the index
+    groupA = team_data.iloc[[0, 2, 4, 6, 8, 10], :]
     groupA = groupA.set_index([pd.Index([0, 1, 2, 3, 4, 5]), ])
     groupA.name = 'GROUP A'
-    groupB = pot_data.iloc[[1, 3, 5, 7, 9, 11], :]
+    groupB = team_data.iloc[[1, 3, 5, 7, 9, 11], :]
     groupB = groupB.set_index([pd.Index([0, 1, 2, 3, 4, 5]), ])
     groupB.name = 'GROUP B'
 
-    qualified = pot_data.iloc[:1, :]
+    # Making a dummy one row data frame to attach winners to
+    qualified = team_data.iloc[:1, :]
 
     print("\nROUND 3\n")
 
+    # Running round 3, function not used to draw groups as it is only 2 groups
     for group in [groupA, groupB]:
         print(group.name, "\n")
         print(group)
-        # time.sleep(1)
+        time.sleep(1)
 
+        # Running the matches for the current group, as seen in round 2
         group = GRP6HA(group)
-        # time.sleep(1)
+        time.sleep(1)
+
+        # Sorting and then displaying current group, as seen in round 2
         group = group.sort_values(['Pts', 'GD', 'GF', 'GA'], ascending=[False, False, False, True])
         group = group.reset_index()
         group = group.drop(['index'], axis=1)
         print("\n", group.to_string(columns=['Country', 'P', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Pts'], index=False),
               "\n")
 
-        top2 = group.iloc[0:2, :]
-        qualified = pd.concat([qualified, top2])
+        # Top 2 become winners and qualify straight to world cup, they are attached to dummy data frame
+        winners = group.iloc[0:2, :]
+        qualified = pd.concat([qualified, winners])
+
+        # 3rd place teams get reattached to main data frame for round 4
         round4 = group.iloc[2:3, :]
-        pot_data = pd.concat([pot_data, round4])
+        team_data = pd.concat([team_data, round4])
+        input("Press enter to continue: ")
 
-        uc = input("Press enter to continue: ")
-
-    pot_data = pot_data.iloc[12:, :]
-    pot_data = pot_data.set_index([pd.Index([0, 1]), ])
+    # Cutting main data frame down to only round 4 teams and reindexing
+    team_data = team_data.iloc[12:, :]
+    team_data = team_data.set_index([pd.Index([0, 1]), ])
 
     print("\nROUND 4\n")
 
-    ict = TLKO(1, pot_data, pot_data, 0.5)
+    # ict(Inter-continental team) runs the two-legged knockout, as seen in round 1, then leaves only the winner in
+    # the data frame
+    ict = TLKO(1, team_data, team_data, 0.5)
     ict = ict.iloc[2:, :]
+
+    # Removing the dummy row from the data frame leaving the teams qualified directly for the world cup
     qualified = qualified.iloc[1:, :]
+
+    # Printing all the qualified teams, checks for host and displays them if they're in AFC
     print("\nQUALIFIED FOR WORLD CUP\n")
     print(qualified.to_string(columns=['Country'], index=False))
     print("\nQUALIFIED FOR INTERCONTINENTAL PLAYOFF\n")
@@ -151,6 +187,7 @@ def afc(time_delay):
         print("\nQUALIFIED AS HOST\n")
         print(host)
 
+    # Returns the team data for the qualified and ict team to the main world cup
     return qualified, ict
 
 
