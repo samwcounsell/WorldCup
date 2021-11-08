3ac0ee2903a61e1c68e749b571c73fc7d188c0f8
Sam <samwcounsell@gmail.com>
2021-11-08 20:21:08 +0000

    Improved data collection, renamed one duplicate player

--- Python_Files/World_Cup_Simulation.py
+++ Python_Files/World_Cup_Simulation.py
@@ -25,7 +25,7 @@ pd.set_option('display.max_columns', None)
 complete_player_data = pd.read_csv("Empty_Player_Data.csv")
 complete_nation_data = pd.read_csv("Empty_Nation_Data.csv")
 
-complete_nation_data = complete_nation_data.set_index("Country")
+# complete_nation_data = complete_nation_data.set_index("Country")
 
 # Welcome to the world cup
 print("\nWelcome to the Python World Cup, produced by Samwcounsell & Githubkeano\n")
@@ -42,6 +42,8 @@ runs = input("Number of runs: \n")
 runs = int(runs)
 
 for i in range(runs):
+
+    complete_nation_data = complete_nation_data.set_index("Country")
 
     if runs != 1:
         time_delay = 0
@@ -135,7 +137,6 @@ for i in range(runs):
 
     # Adding data
     wct = world_cup_teams['Country'].to_numpy()
-    print(wct)
     l = len(wct)
     for i in range(l):
         complete_nation_data.loc[wct[i], "World_Cup_Apps"] = complete_nation_data.loc[wct[i], "World_Cup_Apps"] + 1
@@ -384,7 +385,22 @@ for i in range(runs):
     champion = champion.loc[0, 'Country']
     print("The World Cup Winner is", champion)
 
-    complete_nation_data.loc[champion, "World_Cup_Wins"] = complete_nation_data.loc[champion, "World_Cup_Wins"] + 1
+    complete_nation_data.loc[champion, "World_Cup_Wins"] = complete_nation_data.loc[champion, "World_Cup_Wins"] + 11
+
+    complete_nation_data['Total_P'] = complete_nation_data['Total_P'].add(nation_data['total_P'],
+                                                                                                fill_value=0)
+    complete_nation_data['Total_GF'] = complete_nation_data['Total_GF'].add(nation_data['total_GF'],
+                                                                                                fill_value=0)
+    complete_nation_data['Total_GA'] = complete_nation_data['Total_GA'].add(nation_data['total_GA'],
+                                                                                                fill_value=0)
+    complete_nation_data['Total_Clean_Sheets'] = complete_nation_data['Total_Clean_Sheets'].add(nation_data['total_CS'],
+                                                                                                fill_value=0)
+
+    complete_nation_data = complete_nation_data.reset_index()
+
+    complete_nation_data['GF_PG'] = complete_nation_data['Total_GF'] / complete_nation_data['Total_P']
+    complete_nation_data['GA_PG'] = complete_nation_data['Total_GA'] / complete_nation_data['Total_P']
+    complete_nation_data['Clean_Sheet_%'] = complete_nation_data['Total_Clean_Sheets'] / complete_nation_data['Total_P']
 
     player_data = player_data.reset_index()
     nation_data = nation_data.reset_index()
@@ -398,6 +414,11 @@ for i in range(runs):
     complete_player_data['Total_WC_Goals'] = complete_player_data['Total_WC_Goals'].add(player_data['WC_Goals'], fill_value=0)
     complete_player_data['Total_WC_Assists'] = complete_player_data['Total_WC_Assists'].add(player_data['WC_Assists'],
                                                                                       fill_value=0)
+
+    complete_player_data['G_PG'] = complete_player_data['Total_Goals'] / complete_player_data['Total_P']
+    complete_player_data['A_PG'] = complete_player_data['Total_Assists'] / complete_player_data['Total_P']
+    complete_player_data['WC_G_PG'] = complete_player_data['Total_WC_Goals'] / complete_player_data['Total_WC_P']
+    complete_player_data['WC_A_PG'] = complete_player_data['Total_WC_Assists'] / complete_player_data['Total_WC_P']
 
     # The Awards
 
@@ -441,3 +462,4 @@ nation_data.to_csv('../DashApp/Nation_Data_Set.csv')
 awards_data.to_csv('../DashApp/Award_Data_Set.csv')
 
 complete_nation_data.to_csv('../DashApp/CND_Data_Set.csv')
+complete_player_data.to_csv('../DashApp/CPD_Data_Set.csv')
